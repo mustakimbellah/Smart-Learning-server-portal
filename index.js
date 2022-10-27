96a8ddf3c34a889bba3bb9df23e5eeabfeb52a13
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2022-10-27 20:16:37 +0600

    ready data

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+
+app.get('/', (req, res) => {
+    res.send('News API Running');
+});
+
+app.get('/news', (req, res) => {
+    res.send(categories)
+});
+
+
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = categories.find(n => n._id === id);
+    res.send(selectedNews);
+});
+
+app.listen(port, () => {
+    console.log('Dragon News Server running on port', port);
+})
